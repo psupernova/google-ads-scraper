e5082c19f682fd2c9917a18589ab8c839cb6a66e
Portal <portal.supernova.adm@gmail.com>
2024-11-26 18:07:59 -0300

    Configurado porta corretamente

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn google_scraper:app
+web: gunicorn google_scraper:app --bind 0.0.0.0:$PORT
--- google_scraper.py
+++ google_scraper.py
@@ -363,4 +363,5 @@ def scrape_ads():
         }), 500
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.getenv('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
